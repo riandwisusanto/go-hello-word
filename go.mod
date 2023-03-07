4ac63621fa562c85af5bb6a6cd2bf46db6862ae3
rian <riandwisusanto03@gmail.com>
2023-03-07 13:38:42 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/riandwisusanto/go-hello-word
+
+go 1.20
---
+++ go_say.go
@@ -0,0 +1,9 @@
+package go_hello_word
+
+func SayHello() string {
+	return "Hello Word Upgrade"
+}
+
+func HelloName(name string) {
+	return
+}
